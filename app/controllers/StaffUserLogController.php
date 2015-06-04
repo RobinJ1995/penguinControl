d0e9b399e3d5d1f173e0befbe9b081d59b609bc1
= <robin@robinj.be>
2015-06-04 10:46:13 +0200

    formatting

--- app/controllers/StaffUserLogController.php
+++ app/controllers/StaffUserLogController.php
@@ -4,17 +4,17 @@ class StaffUserLogController extends BaseController
 {
 
 	private $boekhoudingBetekenis = array
-	(
-		-1 => 'Niet te factureren',
-		0 => 'Nog te factureren',
-		1 => 'Gefactureerd'
+	    (
+	    -1 => 'Niet te factureren',
+	    0 => 'Nog te factureren',
+	    1 => 'Gefactureerd'
 	);
 
 	public function index ()
 	{
 		$userlogs = UserLog::orderBy ('time')
-		    ->with ('userInfo.user')
-		    ->paginate ();
+			->with ('userInfo.user')
+			->paginate ();
 
 		$searchUrl = action ('StaffUserLogController@search');
 		$boekhoudingBetekenis = $this->boekhoudingBetekenis;
@@ -35,27 +35,26 @@ class StaffUserLogController extends BaseController
 		$pagination = Input::get ('pagination');
 
 		$query = UserLog::with ('userInfo.user')
-			->whereHas ('userInfo',
-				function ($q) use ($username, $name, $email, $schoolnr)
-				{
-					$q->where ('validated', '1')
+			->whereHas ('userInfo', function ($q) use ($username, $name, $email, $schoolnr)
+			{
+				$q->where ('validated', '1')
 					->where ('username', 'LIKE', '%' . $username . '%')
 					->where (DB::raw ('CONCAT (fname, " ", lname)'), 'LIKE', '%' . $name . '%')
 					->where ('email', 'LIKE', '%' . $email . '%')
 					->where ('schoolnr', 'LIKE', '%' . $schoolnr . '%');
-				}
-			);
+			}
+		);
 
-		if (! empty ($time_van) && !empty ($time_tot))
+		if (!empty ($time_van) && !empty ($time_tot))
 		{
 			$query->whereBetween ('time', array ($time_van, $time_tot));
 		}
 		else
 		{
-			if (! empty ($time_van))
+			if (!empty ($time_van))
 				$query->where ('time', '>', $time_van);
 
-			if (! empty ($time_tot))
+			if (!empty ($time_tot))
 				$query->where ('time', '<', $time_tot);
 		}
 
@@ -89,59 +88,60 @@ class StaffUserLogController extends BaseController
 		{
 			$userLogsIds = Input::get ('userLogId');
 			$boekhouding = Input::get ('boekhouding');
-			
+
 			$userLogs = UserLog::whereIn ('id', $userLogsIds);
 		}
 		else
+		{
 			$alert = 'Geen wijzigingen doorgevoerd';
+		}
 
 		if (isset ($userLogs))
 		{
-			if(! empty (Input::get ('facturatie')))
+			if (! empty (Input::get ('facturatie')))
 			{
 				$userLogs->update (array ('boekhouding' => $boekhouding));
 				$alert = 'Facturatie(s) gewijzigd';
 			}
-			
-			if(! empty (Input::get ('export')))
+
+			if (! empty (Input::get ('export')))
 			{
 				$boekhoudingBetekenis = $this->boekhoudingBetekenis;
 				return View::make ('staff.user.log.export', compact ('userLogsIds', 'boekhoudingBetekenis'));
 			}
 		}
-		
+
 		return Redirect::to ('/staff/user/log')->with ('alerts', array (new Alert ($alert, 'success')));
-		
 	}
-	
+
 	public function export ()
 	{
-		if (! empty (Input::get ('userLogId')))
+		if (!empty (Input::get ('userLogId')))
 		{
 			$userLogsIds = json_decode (Input::get ('userLogId'));
 			$userLogs = UserLog::whereIn ('id', $userLogsIds);
 		}
-		
+
 		$boekhouding = Input::get ('boekhouding');
 		$exportSeperator = Input::get ('seperator');
-		
+
 		$csvHeader = array
-			(
-				'userInfo.id'=>'id',
-				'userInfo.username'=>'gebruikersnaam',
-				'userInfo.fname'=>'voornaam',
-				'userInfo.lname'=>'achternaam',
-				'userInfo.email'=>'e-mail',
-				'userInfo.schoolnr'=>'r-nummer',
-				'userInfo.lastchange'=>'lastchange',
-				'userInfo.validated'=>'validated',
-				'userLog.id'=>'user_log_id',
-				'userLog.time'=>'datum/tijd',
-				'userLog.nieuw'=>'nieuw',
-				'userLog.boekhouding'=>'boekhouding'
-			);
-		
-		
+		(
+			'userInfo.id' => 'id',
+			'userInfo.username' => 'gebruikersnaam',
+			'userInfo.fname' => 'voornaam',
+			'userInfo.lname' => 'achternaam',
+			'userInfo.email' => 'e-mail',
+			'userInfo.schoolnr' => 'r-nummer',
+			'userInfo.lastchange' => 'lastchange',
+			'userInfo.validated' => 'validated',
+			'userLog.id' => 'user_log_id',
+			'userLog.time' => 'datum/tijd',
+			'userLog.nieuw' => 'nieuw',
+			'userLog.boekhouding' => 'boekhouding'
+		);
+
+
 		$fields = Input::get ('exportFields');
 
 		$output = array ();
@@ -149,7 +149,7 @@ class StaffUserLogController extends BaseController
 
 		if ($boekhouding != 'unchanged')
 			$userLogs->update (array ('boekhouding' => $boekhouding));
-		
+
 		switch ($exportSeperator)
 		{
 			case 0:
@@ -164,9 +164,9 @@ class StaffUserLogController extends BaseController
 		}
 
 		foreach ($fields as $field)
-			$userOutput[]= $csvHeader[$field];
-		
-		$output[]= implode($seperator, $userOutput);
+			$userOutput[] = $csvHeader[$field];
+
+		$output[] = implode ($seperator, $userOutput);
 
 		foreach ($userLogsUserInfo as $user_log)
 		{
@@ -175,64 +175,64 @@ class StaffUserLogController extends BaseController
 
 			foreach ($fields as $field)
 			{
-				if ( array_key_exists ($field, $csvHeader))
+				if (array_key_exists ($field, $csvHeader))
 				{
-					$arr =  explode('.', $field);
+					$arr = explode ('.', $field);
 					$table = $arr[0];
 					$tableField = $arr[1];
 					// escape values with double quotes
-					$userOutput[] = '"'.${$table}->{$tableField}.'"';
+					$userOutput[] = '"' . ${$table}->{$tableField} . '"';
 				}
 			}
 
-			$output[]= implode($seperator, $userOutput);
+			$output[] = implode ($seperator, $userOutput);
 		}
 
 		$csvOutput = rtrim (implode (PHP_EOL, $output), "\n");
-		$fileName = 'export/sin_facturatie_'.date('Y_m_d_H_i_s').'.csv';
-		$fileHandle = fopen($fileName, 'w');
+		$fileName = 'export/sin_facturatie_' . date ('Y_m_d_H_i_s') . '.csv';
+		$fileHandle = fopen ($fileName, 'w');
 		fwrite ($fileHandle, $csvOutput);
 		fclose ($fileHandle);
-		$fileLink = '<a href="/'.$fileName.'" target="_blank">'.$fileName.'</a>';
+		$fileLink = '<a href="/' . $fileName . '" target="_blank">' . $fileName . '</a>';
 		$alert = 'Facturatie(s) gewijzigd en ge&euml;xporteerd';
-		
-		return Redirect::to ('/staff/user/log')->with ('alerts', array (
-		    new Alert ($alert, 'success'),
-		    new Alert ('CSV-bestand kan hier worden gedownload: ' . $fileLink, 'info')
-		    ));
-			
+
+		return Redirect::to ('/staff/user/log')->with ('alerts', array
+			(
+				new Alert ($alert, 'success'),
+				new Alert ('CSV-bestand kan hier worden gedownload: ' . $fileLink, 'info')
+			)
+		);
 	}
 
 	public function create ()
 	{
 		$users = array ();
-		
+
 		foreach (UserInfo::orderBy ('username')->get () as $userInfo)
 			$users[$userInfo->id] = $userInfo->username . ' (' . $userInfo->getFullName () . ', ' . $userInfo->schoolnr . ')';
-		
+
 		$boekhoudingBetekenis = $this->boekhoudingBetekenis;
-		
+
 		return View::make ('staff.user.log.create', compact ('users', 'boekhoudingBetekenis'));
 	}
 
 	public function store ()
 	{
 		$validator = Validator::make
-		(
-			array
-			(
-				'user_info_id' => Input::get ('user_info_id'),
-				'Datum/tijd' => Input::get ('time'),
-				'Nieuw' => Input::get ('nieuw'),
-				'Gefactureerd' => Input::get ('boekhouding')
-			),
-			array
-			(
-				'user_info_id' => array ('required', 'numeric'),
-				'Datum/tijd' => array ('required'),
-				'Nieuw' => array ('required'),
-				'Gefactureerd' => array ('required')
-			)
+				(
+				array
+			    (
+			    'user_info_id' => Input::get ('user_info_id'),
+			    'Datum/tijd' => Input::get ('time'),
+			    'Nieuw' => Input::get ('nieuw'),
+			    'Gefactureerd' => Input::get ('boekhouding')
+				), array
+			    (
+			    'user_info_id' => array ('required', 'numeric'),
+			    'Datum/tijd' => array ('required'),
+			    'Nieuw' => array ('required'),
+			    'Gefactureerd' => array ('required')
+				)
 		);
 
 		if ($validator->fails ())
@@ -253,7 +253,7 @@ class StaffUserLogController extends BaseController
 	public function edit ($userlog)
 	{
 		$boekhoudingBetekenis = $this->boekhoudingBetekenis;
-		
+
 		return View::make ('staff.user.log.edit', compact ('userlog', 'boekhoudingBetekenis'));
 	}
 
@@ -273,7 +273,7 @@ class StaffUserLogController extends BaseController
 
 		if ($validator->fails ())
 			return Redirect::to ('/staff/user/log/' . $userlog->id . '/edit')->withInput ()->withErrors ($validator);
-		
+
 		$userlog->boekhouding = Input::get ('boekhouding');
 		$userlog->save ();
 
