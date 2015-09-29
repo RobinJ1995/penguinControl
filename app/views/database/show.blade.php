99a705edae59786ec38793a894d9f49b83dda8b5
Robin Jacobs <r0446734@student.thomasmore.be>
2015-09-29 15:38:19 +0200

    HTTPS geforceerd bij PHPMyAdmin

--- app/views/database/show.blade.php
+++ app/views/database/show.blade.php
@@ -10,7 +10,7 @@ Databases
 		<p>Voor het beheren van de databases maakt SIN gebruik van PHPMyAdmin. Hier kan je databases aanmaken en beheren voor gebruik in je dynamische toepassingen.</p>
 	</div>
 	<div class="large-4 medium-5 small-12 column">
-		<form action="http://sql.sinners.be/" method="POST" id="phpMyAdminPost">
+		<form action="https://phpmyadmin.sinners.be/" method="POST" id="phpMyAdminPost">
 			<input type="hidden" name="pma_username" value="{{ htmlentities ($dbUsername) }}" />
 			<input type="hidden" name="pma_password" value="{{ htmlentities ($dbPassword) }}" />
 			<button>Ga naar PHPMyAdmin</button>
