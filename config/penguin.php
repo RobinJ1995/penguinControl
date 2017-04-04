992df2303a94f9c811d1945c1a952050b9388d9c
Robin Jacobs <robin@robinj.be>
2017-04-04 16:29:48 +0100

    Ability to override config from .env

--- config/penguin.php
+++ config/penguin.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-	'user_registration' => true,
-	'admin_email' => 'root@localhost',
-	'phpmyadmin_url' => 'http://localhost/phpmyadmin',
-	'vhost' => true,
-	'ftp' => true,
-	'database' => true,
-	'mail' => true,
-	'mail_user' => true,
-	'mail_forward' => true,
-	'website' => false
+	'user_registration' => env ('USER_REGISTRATION', true),
+	'admin_email' => env ('ADMIN_EMAIL', 'root@localhost'),
+	'phpmyadmin_url' => env ('PHPMYADMIN_URL', 'http://localhost/phpmyadmin'),
+	'vhost' => env ('VHOST', true),
+	'ftp' => env ('FTP', true),
+	'database' => env ('DATABASE', true),
+	'mail' => env ('MAIL', true),
+	'mail_user' => env ('MAIL_USER', true),
+	'mail_forward' => env ('MAIL_FORWARD', true),
+	'website' => env ('WEBSITE', false)
 ];
