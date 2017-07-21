18a154b813794e717ae4aa9301e9c2b2f4bf0b6e
Dieseled-UP <bourne.denis@gmail.com>
2017-07-21 17:31:39 +0100

    trying to seed .env file

--- app/Providers/PluginViewPathServiceProvider.php
+++ app/Providers/PluginViewPathServiceProvider.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: town10
- * Date: 10/07/2017
- * Time: 10:17
- */
 
 namespace App\Providers;
 
--- resources/plugins/towncms-installer/TownCMSManager.php
+++ resources/plugins/towncms-installer/TownCMSManager.php
@@ -18,14 +18,17 @@ class TownCMSManager
 	{
 		$username = $this->vhost->user->userInfo->username;
 		$domain   = $this->vhost->servername;
-		$homedir = $this->vhost->docroot;
+		$docroot  = $this->vhost->docroot;
 
-		$user = substr($domain, 0, strrpos($domain,"."));
+		$user       = substr ($domain, 0, strrpos ($domain, "."));
 		$dbUsername = $username . '_' . $user;
+
+		echo $docroot . PHP_EOL;
 
 		echo $dbUsername . PHP_EOL;
 
-		$dirPath = substr($homedir, 0, strrpos($homedir, "/", -2));
+		$parts   = explode ('/', $docroot);
+		$dirPath = implode ('/', array_slice ($parts, 0, 4));
 
 		echo $dirPath . PHP_EOL;
 
@@ -38,10 +41,11 @@ class TownCMSManager
 			die ('Username needs to be between 5 and 16 characters long');
 
 		echo 'Generating password and key...' . PHP_EOL;
-		$password      = bin2hex (openssl_random_pseudo_bytes (10));
-		$passwordCrypt = password_hash ($password, PASSWORD_DEFAULT);
-		$key           = bin2hex (openssl_random_pseudo_bytes (32));
-		$sedMap        = [
+		$password = "***REMOVED***"/*bin2hex (openssl_random_pseudo_bytes (10))*/
+		;
+		/*$passwordCrypt = password_hash ($password, PASSWORD_DEFAULT);*/
+		$key    = bin2hex (openssl_random_pseudo_bytes (32));
+		$sedMap = [
 			'{:DOMAIN:}'   => $domain,
 			'{:USERNAME:}' => $username,
 			'{:PASSWORD:}' => $password,
@@ -57,30 +61,25 @@ class TownCMSManager
 		$pdo->exec ("CREATE DATABASE `$dbUsername`;");
 		$pdo->exec ("GRANT ALL PRIVILEGES ON `$dbUsername`.* TO '$username'@'%';");
 
-		// Remove the public folder from the document root
-		echo $homedir . PHP_EOL;
+		echo exec ('pwd') . PHP_EOL;
 
-		$switchDir = 'cd ' . $homedir;
+		if ( ! chdir ("../../Documents"))
+			die ("Could not switch directory...");
 
-		exec ($switchDir);
-
-		/*if ( ! chdir ("$dirPath"))
-			die ("Switching to $dirPath failed");*/
+		echo exec ('pwd') . PHP_EOL;
 
 		echo 'Cloning Git repository...' . PHP_EOL;
 		unset ($output, $exitCode);
-		exec ('GIT_SSH_COMMAND="ssh -i ' . escapeshellarg (SSH_KEY) . '" git clone ' . escapeshellarg (TOWN_CMS_GIT_REPO), $output, $exitCode). ' sudo -u ' . escapeshellarg ($username);
+		exec ('GIT_SSH_COMMAND="ssh -i ' . escapeshellarg (SSH_KEY) . '" git clone ' . escapeshellarg (TOWN_CMS_GIT_REPO), $output, $exitCode) . ' sudo -u ' . escapeshellarg ($username);
 		if ($exitCode !== 0)
 			die ('Cloning Git repository failed...' . PHP_EOL . implode (PHP_EOL, $output));
 
+		if ( ! chdir ("town-cms"))
+			die ("Switching to town-cms failed");
+
+		echo exec ('pwd') . PHP_EOL;
+
 		echo 'Installing dependencies...' . PHP_EOL;
-
-		$townCMS = 'cd town-cms';
-		/*if ( ! chdir ("$homedir/town-cms"))
-			die ("Switching to $homedir/town-cms failed");*/
-
-		exec ($townCMS);
-
 		unset ($output, $exitCode);
 		exec ('composer install', $output, $exitCode);
 		if ($exitCode !== 0)
@@ -92,21 +91,37 @@ class TownCMSManager
 		if ($exitCode !== 0)
 			die ('Copying .env file failed...' . PHP_EOL . implode (PHP_EOL, $output));
 
+		echo 'Generate application key...' . PHP_EOL;
 		unset ($output, $exitCode);
 		exec ('php artisan key:generate', $output, $exitCode);
 		if ($exitCode !== 0)
 			die ('`php artisan key:generate` failed...' . PHP_EOL . implode (PHP_EOL, $output));
 
-		foreach ($sedMap as $placeholder => $value) {
-			$sedStr = 's/' . $placeholder . '/' . $value . '/g';
+		echo exec ('pwd') . PHP_EOL;
 
-			unset ($output, $exitCode);
-			exec ('sed -i ' . escapeshellarg ($sedStr) . ' .env', $output, $exitCode);
-			if ($exitCode !== 0)
-				die ('Placeholder replacement with `sed` failed...' . PHP_EOL . implode (PHP_EOL, $output));
+		echo 'Setting up the CMS...' . PHP_EOL;
+
+		if (file_exists ('.env'))
+		{
+
+			foreach ($sedMap as $placeholder => $value)
+			{
+
+				$sedStr = 's/' . $placeholder . '/' . $value . '/g';
+
+				echo $sedStr . PHP_EOL;
+
+				unset ($output, $exitCode);
+				exec ('sudo sed ' . escapeshellarg ($sedStr) . ' .env > .env', $output, $exitCode);
+				if ($exitCode !== 0)
+					die ('Placeholder replacement with `sed` failed...' . PHP_EOL . implode (PHP_EOL, $output));
+			}
+
+		} else {
+			die ('Could not find file' . PHP_EOL . implode (PHP_EOL, $output));
 		}
 
-		echo 'Generating vHost...' . PHP_EOL;
+		/*echo 'Generating vHost...' . PHP_EOL;
 		unset ($output, $exitCode);
 		exec ('sudo cp /etc/apache2/sites-available/template.conf ' . escapeshellarg ('/etc/apache2/sites-available/' . $user . '.conf'), $output, $exitCode);
 		if ($exitCode !== 0)
@@ -137,7 +152,7 @@ class TownCMSManager
 		unset ($output, $exitCode);
 		exec ('sudo systemctl reload apache2', $output, $exitCode);
 		if ($exitCode !== 0)
-			die ('Reloading Apache failed...' . PHP_EOL . implode (PHP_EOL, $output));
+			die ('Reloading Apache failed...' . PHP_EOL . implode (PHP_EOL, $output));*/
 
 		echo PHP_EOL . '---' . PHP_EOL;
 		echo 'Setup completed!' . PHP_EOL;
@@ -153,5 +168,50 @@ class TownCMSManager
 			'exitcode' => $exitCode,
 			'output'   => implode (PHP_EOL, $output)
 		);
+	}
+
+	protected function changeEnv ($data = array())
+	{
+		if (count ($data) > 0) {
+
+			// Read .env-file
+			$env = file_get_contents (base_path () . '/.env');
+
+			// Split string on every " " and write into array
+			$env = preg_split ('/\s+/', $env);;
+
+			// Loop through given data
+			foreach ((array)$data as $key => $value) {
+
+				// Loop through .env-data
+				foreach ($env as $env_key => $env_value) {
+
+					// Turn the value into an array and stop after the first split
+					// So it's not possible to split e.g. the App-Key by accident
+					$entry = explode ("=", $env_value, 2);
+
+					// Check, if new key fits the actual .env-key
+					if ($entry[0] == $key) {
+						// If yes, overwrite it with the new one
+						$env[$env_key] = $key . "=" . $value;
+					}
+					else {
+						// If not, keep the old one
+						$env[$env_key] = $env_value;
+					}
+				}
+			}
+
+			// Turn the array back to an String
+			$env = implode ("\n", $env);
+
+			// And overwrite the .env with the new data
+			file_put_contents (base_path () . '/.env', $env);
+
+			return TRUE;
+		}
+		else {
+			return FALSE;
+		}
 	}
 }
